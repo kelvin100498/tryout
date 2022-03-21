3f62179c79a4c13814145d1bf3535614fb5f336a
kelvin100498 <kelvinadityanouval05@gmail.com>
2022-03-21 21:00:10 +0700

    update

---
+++ Answer.js
@@ -0,0 +1,32 @@
+// let anagram = 'aaz, zza';
+// let anagramPisah =anagram.split(',');
+
+// function anagramCheck(a, b) {
+//   let panjangA = a.length;
+//   let panjangB = b.length;
+
+//   if (panjangA !== panjangB) {
+//     return false;
+//   }
+//   let string1 = a.split('').sort().join('');
+//   let string2 = b.split('').sort().join('');
+  
+
+//   if (string1 === string2) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(anagramCheck('nagram', 'nagaram'));
+
+let a= 10
+let b = 5
+
+a = a + b   
+b = a - b
+a = a - b
+
+
+
