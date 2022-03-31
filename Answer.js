696c2419024482b19a020c4101681d3cad7c9929
Kelvin Aditya Nouval <50832597+kelvin100498@users.noreply.github.com>
2022-03-31 22:06:49 +0700

    update

--- Answer.js
+++ Answer.js
@@ -21,12 +21,38 @@
 
 // console.log(anagramCheck('nagram', 'nagaram'));
 
-let a= 10
-let b = 5
+// let a= 10
+// let b = 5
 
-a = a + b   
-b = a - b
-a = a - b
+// a = a + b   
+// b = a - b
+// a = a - b
+
+//TO kedua
+//Cases :
+//- create a function that accept single parameter. the parameter will be an array of integer
+//- your function should be able to count the unique value inside the array
+//- examples:
+// - `countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]))` => 7
+//- `countUniqueValues([1, 2, 2, 2, 2, 2, 2, 2, 4, 6]))` => 4
+//- `countUniqueValues([]))` => 0
+
+// const Unik = (a) => {
+//   let test = [...new Set(a)].length
+//   return test
+// };
+
+// console.log(Unik([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
+
+const Unik = (a) => {
+  for (let i = 0; i <= a.length; i++) {
+    if (a.indexOf(a[i]) === i) {
+      console.log(a[i]);
+    }
+  }
+};
+console.log(Unik([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
+
 
 
 
